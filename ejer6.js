ee61f7041778365688c5be3dc0831054157b1d02
Rogelio2107 <79059764+Rogelio2107@users.noreply.github.com>
2023-06-15 13:17:01 -0600

    ejerciciosjs

---
+++ ejer6.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = function(nums) {
+    let n = nums.length;
+    let c = 0;
+    for (let i = 0; i < n; i++) {
+        c = c ^ nums[i];
+    }
+    return c; 
+};
